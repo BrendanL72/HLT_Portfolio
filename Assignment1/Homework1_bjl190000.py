1ea48b8bfde6a608acc6ad8148726c06e75a20c1
BrendanL <BrendanL72@users.noreply.github.com>
2023-02-04 04:57:05 -0600

    hi

--- Assignment1/Homework1_bjl190000.py
+++ Assignment1/Homework1_bjl190000.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pickle
 import csv
+import re
 
 class Person:
    def __init__(self, first_name, last_name, middle_name, id, phone_num):
@@ -24,6 +25,52 @@ class Person:
             """ 
       print(display_format.format(**format_vars))
 
+def stan_last_name(last_name):
+   return last_name.capitalize()
+
+def stan_first_name(first_name):
+   return first_name.capitalize()
+
+def stan_mid_init(mid_init):
+   default_mid_init = "X"
+   if len(mid_init):
+      return mid_init[0].capitalize()
+   else:
+      return default_mid_init
+
+def stan_id(id, ):
+   id_regex = "[A-Z]{2}\d{4}"
+   if re.fullmatch(id_regex, id):
+      pass
+   else:
+      new_id = ""
+      while not re.fullmatch(id_regex, new_id):
+         new_id = input("ID Error for Previous ID {}, please enter an ID with 2 capital letters followed by 4 numbers\n".format(id))
+      id = new_id
+   return id
+
+
+def stan_phone_num(phone_num):
+   phone_num_format = "\d{3}[-]\d{3}[-]\d{4}"
+   if re.fullmatch(phone_num_format, phone_num):
+      pass
+   else:
+      raw_phone_num = re.sub("[^0-9]", "", phone_num)
+      new_phone_num = raw_phone_num[0:3] + "-" + raw_phone_num[3:6] + "-" + raw_phone_num[6:10]
+      phone_num = new_phone_num
+   return phone_num
+
+
+def standardize_info(last_name, first_name, mid_init, id, phone_num):
+   last_name = stan_last_name(last_name)
+   first_name = stan_first_name(first_name)
+   mid_init = stan_mid_init(mid_init)
+   id = stan_id(id)
+   phone_num = stan_phone_num(phone_num)
+   
+   return (last_name, first_name, mid_init, id, phone_num)
+
+
 #  process_input_file gets a formatted fully populated input file path which contains information about people
 #  It returns a dictionary of Person objects where the person's ID is the key
 #  It also standardizes any data to be in the proper format (ex. change first letter of first name to be capitalized)
@@ -36,13 +83,13 @@ def process_input_file(input_file_path):
       for row in input_file_reader:
          last_name, first_name, mid_init, id, phone_num = row
          #standardize and check id 
-         standardize_info()
+         last_name, first_name, mid_init, id, phone_num = standardize_info(last_name, first_name, mid_init, id, phone_num)
          #add stuff
-         persons[id] = Person(last_name, first_name, mid_init, id, phone_num)
+         if id not in persons.keys():
+            persons[id] = Person(last_name, first_name, mid_init, id, phone_num)
    return persons
 
-def standardize_info():
-   pass
+
 
 if __name__ == "__main__":
    args_list = sys.argv
